893f2c0f73327ae73f56eb585d786a31f8bacaa9
Jem Appru <jem@supapass.com>
2019-01-31 17:15:06 +0000

    new learning init

--- 2-JS-basics/final/script.js
+++ 2-JS-basics/final/script.js
@@ -1,3 +1,13 @@
+console.log("hello again");
+
+
+
+
+
+
+
+
+
 /*****************************
 * Variables and data types
 */
@@ -137,6 +147,25 @@ Mark and John are trying to compare their BMI (Body Mass Index), which is calcul
 
 GOOD LUCK 😀
 */
+
+var markHeight = 2;
+var markMass= 800;
+
+var johnHeight= 2;
+var johnMass= 100;
+
+var johnBMI = johnMass/(johnHeight*johnHeight);
+var markBMI = markMass/(markHeight*markHeight);
+
+var markBigger = markBMI > johnBMI;
+
+console.log("john= "+johnBMI);
+console.log("mark= "+markBMI);
+
+console.log("mark bigger?" + markBigger);
+
+
+
 /*
 var massMark = 78; // kg
 var heightMark = 1.69; // meters
@@ -306,8 +335,58 @@ John and Mike both play basketball in different teams. In the latest 3 games, Jo
 4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console. HINT: you will need the && operator to take the decision. If you can't solve this one, just watch the solution, it's no problem :)
 5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
 
-GOOD LUCK 😀
 */
+
+//1)
+
+var teamJohn = [89,120,103];
+var teamMike = [116,94,123];
+
+
+var johnAVG = (teamJohn[0]+teamJohn[1]+teamJohn[2])/3;
+
+console.log("John's team average = " + johnAVG);
+
+
+var mikeAVG = (teamMike[0]+teamMike[1]+teamMike[2])/3;
+
+console.log("Mike's team average = "+mikeAVG);
+
+if(johnAVG>mikeAVG){
+    console.log("JOHN IS DA WINNDER");
+}
+if(mikeAVG>johnAVG){
+    console.log("MIKE IS DA WINNDER");
+}
+if(mikeAVG==johnAVG){
+    console.log("IS A DRAW!");
+}
+
+//4)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 var scoreJohn = (189 + 120 + 103) / 3;
 var scoreMike = (129 + 94 + 123) / 3;
@@ -371,7 +450,60 @@ yearsUntilRetirement(1969, 'Jane');
 
 /*****************************
 * Function Statements and Expressions
+
 */
+
+//function delcaration
+function calculateAge(birthYear){
+    
+    return 0;
+    
+}
+
+
+//functions expression
+var whatDoYou = function(job,firstName){
+    
+    switch(job){
+                    
+        case 'teacher':
+            return firstName + ' teaches kid how to code';
+        
+        case ' driver':
+            return firstName + ' drivers yll araround';
+        
+        case 'dog-walker':
+            return firstName + ' walks the dog';
+            
+        default:
+            return firstName + 'is unemploed lmao';
+    
+    }
+    
+    
+}
+
+console.log(whatDoYou('fgh', 'mark'));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 // Function declaration
 // function whatDoYouDo(job, firstName) {}
@@ -450,6 +582,69 @@ In the end, John would like to have 2 arrays:
 
 GOOD LUCK 😀
 */
+
+var johntips = [];
+var johnFinal = [];
+
+function  billCalculator(amount){
+    
+    if(amount< 50){
+        tip= amount*0.2;
+        result = amount - tip
+        johntips.push(tip);
+        johnFinal.push(result);
+        
+    }
+    
+    if(amount>=50 && amount<201){
+        tip =amount*0.25;
+        result=  amount-tip
+        johntips.push(tip);
+        johnFinal.push(result);
+    }
+    
+    if(amount> 202){
+        tip = amount*0.1;
+        result = amount-tip
+        johntips.push(tip);
+        johnFinal.push(result);
+        
+    }
+    
+}
+
+billCalculator(124);
+billCalculator(48);
+billCalculator(268);
+
+console.log("first tip = " + johntips[0]);
+console.log("Second tip = " + johntips[1]);
+console.log("third tip = " + johntips[2]);
+
+console.log("first final amount = " + johnFinal[0]);
+console.log("Second final amount= " + johnFinal[1]);
+console.log("third final amount = " + johnFinal[2]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
 function tipCalculator(bill) {
     var percentage;
