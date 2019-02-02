16fbb9c7db7ed287dcba6d333ebcf116a92bda6f
Jem Appru <j.appru@gmail.com>
2019-02-02 21:26:46 +0000

    dom update

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -9,20 +9,57 @@ GAME RULES:
 
 */
 
-var scores,roundScores,activePlayer,dice;
+var scores,roundScores,activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
 
-dice = Math.floor(Math.random()*6)+1;
 
-
-
-document.querySelector('#current-' + activePlayer) .textContent = dice;
 
 //document.querySelector('#current-' + activePlayer) .innerHTML = '<em>'+ dice +'</em>';
+
+document.querySelector('.dice').style.display = 'none'; 
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll') .addEventListener('click',function(){
+    //1 random number
+   var dice = Math.floor(Math.random()*6)+1;
+    
+    //2 display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-'+dice+'.png';
+    console.log(dice)
+    
+    
+    //3 update the round score - only if !=1
+    if(dice !== 1){
+        //add the score
+         roundScore +=dice;
+        document.querySelector('#current-' + activePlayer) .textContent = roundScore;
+        
+    }else{
+        //next player turn
+        activePlayer === 0 ? activePlayer =1 :
+        activePlayer =0;
+        roundScore = 0;
+        
+        document.getElementById("current-0").textContent = '0';
+        document.getElementById("current-1").textContent = '0';
+        
+        
+    }
+    
+    
+    
+});
 
 
 
