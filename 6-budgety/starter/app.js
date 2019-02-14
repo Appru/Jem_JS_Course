1430b3de98a6791062dd97682c77c15557c3b9cb
Jem Appru <jem@supapass.com>
2019-02-14 17:46:15 +0000

    Update app.js

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -24,19 +24,46 @@ CONTROLLER MODULES = Add even handler
 
 */
 
-var budgetController = (function(){
-    
+var budgetController = (function () {
+
     var x = 23;
-    
-    var add = function(a){
+
+    var add = function (a) {
         return x + a;
     }
-    
-    
+
+
     return {
-        publicTest: function(b){
-            console.log(add(b));
+        publicTest: function (b) {
+           return add(b);
         }
     }
-    
+
 })();
+
+
+var UIController = (function () {
+
+
+    //some code
+
+
+
+})();
+
+
+
+var controller = (function (budgetCtrl, UICtrl) {
+
+var z = budgetCtrl.publicTest(5);
+    
+    return {
+        anotherPublic: function(){
+            console.log(z);
+        }
+    }
+
+
+
+
+})(budgetController, UIController);
