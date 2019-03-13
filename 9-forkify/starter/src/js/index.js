a95956c2c85457e4ad5256320a60c10ea5b217ea
Jem Appru <jem@supapass.com>
2019-03-13 17:50:42 +0000

    finally set up

--- 9-forkify/starter/src/js/index.js
+++ 9-forkify/starter/src/js/index.js
@@ -1,1 +1,7 @@
 // Global app controller
+
+
+import x from './test';
+
+
+console.log(`I imported ${x} form another modules`);
---
+++ 9-forkify/starter/src/js/test.js
@@ -0,0 +1,3 @@
+console.log ('imported moudle');
+
+export default 11111;
