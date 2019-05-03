9ee0353a609b074f837fc36875ab2608d7dc06fb
Jem Appru <jem@supapass.com>
2019-05-03 16:53:22 +0100

    f

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -4,19 +4,22 @@ const url = require("url");
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
 
-const laptop = JSON.parse(json);
-console.log(laptop);
+const laptopData = JSON.parse(json);
+console.log(laptopData);
 
 const server = http.createServer((req, res) => {
+  
   const pathname = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+
 
   if (pathname === "/products" || pathname === "/") {
     res.writeHead(200, { "Content-type": "text/html" });
     res.end("This is da producst page");
   } 
-  else if (pathname === "/laptop") {
+  else if (pathname === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "Content-type": "text/html" });
-    res.end("dis the laptoppages");
+    res.end(`dis the laptoppages for da laptop ${id}`);
   } 
   else {
     res.writeHead(404, { "Content-type": "text/html" });
